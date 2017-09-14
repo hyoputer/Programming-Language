758052763587560b733a20283d7db3e781046d7b
hyoputer <ajtwlsalsdl0@naver.com>
2017-09-14 14:02:43 +0900

    2-1

--- hw1/2.ml
+++ hw1/2.ml
@@ -17,3 +17,16 @@ let _ = print_int(a23)
 let _ = print_endline("")
 let _ = print_int(a24)
 let _ = print_endline("")*)
+let _ =
+    let assert_equal (expected: int) (actual: int) =
+            if expected = actual then print_endline "true"
+                    else Printf.printf "Expected %d but actual %d\n" expected actual
+                        in
+                            let test_sigma (a: int) (b: int) (f: int->int) (expected: int) =
+                                    sigma (a,b,f) |> assert_equal expected
+                                        in
+                                            test_sigma 1 2 (fun x -> x * x) 5;
+                                                test_sigma 1 100 (fun x -> x) 5050;
+                                                    test_sigma 1 1 (fun x -> x) 1;
+                                                        test_sigma 1 0 (fun x -> x) 0;
+                                                            test_sigma 101 200 (fun x -> -x) (-15050);
